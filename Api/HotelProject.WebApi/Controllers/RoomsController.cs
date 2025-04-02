4faad488715685dcab61f4939da9e4bb0f3caa03
Haçik Ulu <hacikd2@gmail.com>
2025-04-02 17:49:45 +0300

    Controllers and Registrations editing.

---
+++ Api/HotelProject.WebApi/Controllers/RoomsController.cs
@@ -0,0 +1,65 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        public RoomsController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            var values = _roomService.TGetAll();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRoom(int id)
+        {
+            var values = _roomService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateRoom(Room room)
+        {
+            _roomService.TAdd(new Room
+            {
+                BathCount = room.BathCount,
+                BedCount = room.BedCount,
+                CoverImage = room.CoverImage,
+                Description = room.Description,
+                Price = room.Price,
+                RoomNumber = room.RoomNumber,
+                Wifi = room.Wifi,
+                Title = room.Title
+            });
+            return Ok("Oda başarıyla oluşturuldu!");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRoom(Room room)
+        {
+            _roomService.TUpdate(room);
+            return Ok("Oda bilgileri başarıyla güncellendi!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRoom(int id)
+        {
+            var values = _roomService.TGetById(id);
+            _roomService.TDelete(values);
+            return Ok("Oda bilgileri başarıyla silindi!");
+        }
+
+    }
+}
---
+++ Api/HotelProject.WebApi/Extensions/ServiceRegistration.cs
@@ -0,0 +1,40 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete.EntityFramework;
+using HotelProject.DataAccessLayer.Concrete.Repositories;
+
+namespace HotelProject.WebApi.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Generic Service
+            services.AddScoped(typeof(IGenericDal<>), typeof(GenericRepository<>));
+
+            // Room Service
+            services.AddScoped<IRoomService, RoomManager>();
+            services.AddScoped<IRoomDal, EfRoomDal>();
+
+            // Service
+            services.AddScoped<IServiceService, ServiceManager>();
+            services.AddScoped<IServiceDal, EfServiceDal>();
+
+            // Staff
+            services.AddScoped<IStaffService, StaffManager>();
+            services.AddScoped<IStaffDal, EfStaffDal>();
+
+            // Subscribe
+            services.AddScoped<ISubscribeService, SubscribeManager>();
+            services.AddScoped<ISubscribeDal, EfSubscribeDal>();
+
+            // Testimonial
+            services.AddScoped<ITestimonialService, TestimonialManager>();
+            services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+
+
+            return services;
+        }
+    }
+}
