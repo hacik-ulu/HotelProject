38445fc7342fcc1ae8edec361b145d27c518d6d4
Haçik Ulu <hacikd2@gmail.com>
2025-04-02 17:02:14 +0300

    Ef methods edited.

---
+++ Api/HotelProject.DataAccessLayer/Concrete/EntityFramework/EfRoomDal.cs
@@ -0,0 +1,14 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete.Database;
+using HotelProject.DataAccessLayer.Concrete.Repositories;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.Concrete.EntityFramework
+{
+    public class EfRoomDal : GenericRepository<Room>, IRoomDal
+    {
+        public EfRoomDal(Context context) : base(context)
+        {
+        }
+    }
+}
--- Api/HotelProject.DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ Api/HotelProject.DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -15,29 +15,24 @@ namespace HotelProject.DataAccessLayer.Concrete.Repositories
         {
             _context = context;
         }
-
         public void Add(T entity)
         {
             _context.Add(entity);
             _context.SaveChanges();
         }
-
         public void Delete(T entity)
         {
             _context.Remove(entity);
             _context.SaveChanges();
         }
-
         public List<T> GetAll()
         {
             return _context.Set<T>().ToList();
         }
-
         public T GetById(int id)
         {
             return _context.Set<T>().Find(id);
         }
-
         public void Update(T entity)
         {
             _context.Update(entity);
