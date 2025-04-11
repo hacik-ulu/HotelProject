bb72051166acd64e7781b221548774206680db21
Haçik Ulu <hacikd2@gmail.com>
2025-04-11 22:33:06 +0300

    Services edited.

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_ServiceComponentPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServiceComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServiceComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly HttpClient _httpClient;
+        public _ServiceComponentPartial(HttpClient httpClient, IConfiguration configuration)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(configuration["ApiSettings:BaseUrl"]);
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var response = await _httpClient.GetAsync("Services");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
