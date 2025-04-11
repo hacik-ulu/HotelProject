7edd2c4b56c5642ef58413efefdc00ccbaf093dc
Haçik Ulu <hacikd2@gmail.com>
2025-04-11 21:49:36 +0300

    Rooms edited.

---
+++ Frontend/HotelProject.WebUI/Dtos/RoomDtos/ResultRoomDto.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelProject.WebUI.Dtos.RoomDtos
+{
+    public class ResultRoomDto
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string CoverImage { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public int BedCount { get; set; }
+        public int BathCount { get; set; }
+        public string Wifi { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsComponentPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _OurRoomsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly HttpClient _httpClient;
+        public _OurRoomsComponentPartial(HttpClient httpClient, IConfiguration configuration)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(configuration["ApiSettings:BaseUrl"]);
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var response = await _httpClient.GetAsync("Rooms");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
