07859a6e54e1244aa6ba299625651713aed2752f
Haçik Ulu <hacikd2@gmail.com>
2025-04-12 22:29:21 +0300

    Staffs edited.

--- Frontend/HotelProject.WebUI/Dtos/StaffDtos/ResultStaffDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/StaffDtos/ResultStaffDto.cs
@@ -5,5 +5,8 @@
         public int StaffId { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
+        public string SocialMediaIcon1 { get; set; }
+        public string SocialMediaIcon2 { get; set; }
+        public string SocialMediaIcon3 { get; set; }
     }
 }
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamComponentPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.StaffDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TeamComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly HttpClient _httpClient;
+        public _TeamComponentPartial(HttpClient httpClient, IConfiguration configuration)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(configuration["ApiSettings:BaseUrl"]);
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var response = await _httpClient.GetAsync("Staffs");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
