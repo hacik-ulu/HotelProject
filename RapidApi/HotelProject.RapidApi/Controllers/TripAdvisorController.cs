30ab3701cb91b07c394e9dbc28d1978e8633c70b
Haçik Ulu <hacikd2@gmail.com>
2025-04-14 17:04:50 +0300

    .

---
+++ RapidApi/HotelProject.RapidApi/Controllers/TripAdvisorController.cs
@@ -0,0 +1,50 @@
+﻿using HotelProject.RapidApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.RapidApi.Controllers
+{
+    public class TripAdvisorController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public TripAdvisorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<TripAdvisorViewModels> tripAdvisorViewModels = new List<TripAdvisorViewModels>();
+
+            var apiKey = _configuration["RapidApi:Key"];
+            var apiHost = _configuration["RapidApi:Host"];
+            var apiUrl = _configuration["RapidApi:Url"];
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(apiUrl),
+                Headers =
+                {
+                    { "x-rapidapi-key", apiKey },
+                    { "x-rapidapi-host", apiHost },
+                },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                // API yanıtını deserialize et
+                var tripAdvisorResponse = JsonConvert.DeserializeObject<TripAdvisorResponseModel>(body);
+
+                // Data listesini alın
+                tripAdvisorViewModels = tripAdvisorResponse?.Data;
+
+                return View(tripAdvisorViewModels);
+            }
+        }
+    }
+}
---
+++ RapidApi/HotelProject.RapidApi/Models/TripAdvisorViewModels.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelProject.RapidApi.Models
+{
+    public class TripAdvisorResponseModel
+    {
+        public string Status { get; set; }
+        public List<TripAdvisorViewModels> Data { get; set; }
+    }
+
+    public class TripAdvisorViewModels
+    {
+        public string HeroImgUrl { get; set; }
+        public string Name { get; set; }
+        public double AverageRating { get; set; }
+        public int UserReviewCount { get; set; }
+    }
+}
