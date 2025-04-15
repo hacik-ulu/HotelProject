0a57e62a9b09860f86ea49e1f838d029b91a8e1f
Haçik Ulu <hacikd2@gmail.com>
2025-04-15 22:30:29 +0300

    UI and RoomDetail edited.

--- Frontend/HotelProject.WebUI/Controllers/RoomDetailController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RoomDetailController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class RoomDetailController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public RoomDetailController(HttpClient httpClient, IConfiguration configuration)
         {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(configuration["ApiSettings:BaseUrl"]);
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var response = await _httpClient.GetAsync($"Rooms/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultRoomDto>(jsonData);
+                return View(value); 
+            }
+
             return View();
         }
+
     }
 }
